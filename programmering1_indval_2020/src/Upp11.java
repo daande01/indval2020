5373cfd0362cf742bef003465461c0e902fe6ce8
daande01 <daniel.evert.andersson@gmail.com>
2020-08-31 15:33:54 +0200

    start

---
+++ programmering1_indval_2020/src/Upp11.java
@@ -0,0 +1,18 @@
+
+public class Upp11 {
+
+	public static void main(String[] args) {
+
+		/*
+		System.out.println("daniel");
+		System.out.println("andersson");
+		System.out.println("berghogen 223");
+		*/
+		System.out.println("daniel \nandersson \nberghogen 223");
+
+
+
+	}
+
+
+}
