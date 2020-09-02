0c1e2abf6f9ad0a207395f5181842b5e4b0aed24
daande01 <daniel.evert.andersson@gmail.com>
2020-09-02 10:29:12 +0200

    start variabler

---
+++ programmering1_indval_2020/src/variabler/Var1.java
@@ -0,0 +1,21 @@
+package variabler;
+
+public class Var1 {
+
+	public static void main(String[] args) {
+
+		int tal; // deklaretat en variabel av datatypen integer (heltal) vid namn tal
+
+		tal=5; // tilldelning
+
+		int tal2=10; // initiering = deklarering och tilldelning på samma rad
+
+		int summa=tal+tal2; // från höger till vänster sida
+
+		System.out.println("summan är :"+summa);
+
+		int prisPåLösgodis ;
+		int prispålösgodis;
+
+	}
+}
---
+++ programmering1_indval_2020/src/variabler/Var2.java
@@ -0,0 +1,23 @@
+package variabler;
+
+public class Var2 {
+
+	public static void main(String[] args) {
+
+
+		double tal1; // dklareing av flyttal (decialtalsvariabel)
+
+		tal1=5.78;
+
+
+		double tal2= 10.5;
+
+		double summa = tal1+ tal2 ;
+
+		System.out.println(summa);
+
+	}
+
+
+
+}
