77962fe19699b56f75600d3f435714368933cdd2
daande01 <daniel.evert.andersson@gmail.com>
2020-09-14 15:55:52 +0200

    new files

---
+++ programmering1_indval_2020/src/Strings/Text2.java
@@ -0,0 +1,32 @@
+package Strings;
+
+public class Text2 {
+
+	public static void main(String[] args) {
+
+
+		String mening ="hej daniel";
+
+		int längd =mening.length();
+		char c=mening.charAt(2);
+		int position=mening.indexOf(' ');
+
+		String ord=mening.substring(4);
+
+
+		System.out.println(längd);
+		System.out.println(c);
+		System.out.println(position);
+		System.out.println(ord);
+
+
+
+
+
+
+
+	}
+
+
+
+}
