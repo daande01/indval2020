006da0951c71c167d25de542aa154e3c16a9cb15
daande01 <daniel.evert.andersson@gmail.com>
2020-09-14 16:24:38 +0200

    new

---
+++ programmering1_indval_2020/src/chars/Tecken1.java
@@ -0,0 +1,22 @@
+package chars;
+
+import javax.swing.JOptionPane;
+
+public class Tecken1 {
+	public static void main(String[] args) {
+
+		String ord="hej";
+		char c = 'h';
+
+		char cc ='\u0045';
+
+		int i = c;
+
+		JOptionPane.showMessageDialog(null, cc);
+
+		System.out.println(i);
+
+
+
+	}
+}
