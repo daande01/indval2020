14fc09d374293972a4298c62683d743a3c6c6c61
daande01 <daniel.evert.andersson@gmail.com>
2020-09-14 15:38:50 +0200

    new

---
+++ programmering1_indval_2020/src/Strings/Text1.java
@@ -0,0 +1,23 @@
+package Strings;
+
+public class Text1 {
+
+	public static void main(String[] args) {
+
+		String ord1="hej";
+		String ord2="då";
+		int i = 5;
+		int j=5;
+
+		String mening= ord1+ord2;
+		String mening2 = "summa :"+(i+j);
+
+		System.out.println(mening);
+		System.out.println(mening2);
+
+
+
+	}
+
+
+}
