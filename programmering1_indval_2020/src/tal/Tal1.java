10b9c0a6f6e2653a4f8db704bd45e64281b9a02d
daande01 <daniel.evert.andersson@gmail.com>
2020-09-14 13:51:22 +0200

    new file

--- programmering1_indval_2020/src/tal/Tal1.java
+++ programmering1_indval_2020/src/tal/Tal1.java
@@ -2,4 +2,22 @@ package tal;
 
 public class Tal1 {
 
+	public static void main(String[] args) {
+
+
+		int i = 10;
+
+		int h =3;
+
+		int kvot = i/h;       //      10/2
+
+		int rest= i%h;
+
+		System.out.println(kvot+"  " +rest);
+
+
+	}
+
+
+
 }
