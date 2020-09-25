6f04d074e489c5335a78f4aa5352da00e8a27d9a
daande01 <daniel.evert.andersson@gmail.com>
2020-09-25 09:56:06 +0200

    ovn

---
+++ programmering1_indval_2020/src/Ovn244.java
@@ -0,0 +1,20 @@
+import javax.swing.JOptionPane;
+
+public class Ovn244 {
+
+	public static void main(String[] args) {
+
+		String s1 =JOptionPane.showInputDialog("ange siffra");
+
+			int tal=Integer.parseInt(s1);
+
+			char c =(char)tal;
+
+			JOptionPane.showMessageDialog(null, c);
+
+
+	}
+
+
+
+}
