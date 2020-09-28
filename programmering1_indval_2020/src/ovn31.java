a8632cdd4c40eb1ea66b86ec052d25f9a8d7ce69
daande01 <daniel.evert.andersson@gmail.com>
2020-09-28 16:29:16 +0200

    new files

---
+++ programmering1_indval_2020/src/ovn31.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ovn31 {
+
+	public static void main(String[] args) {
+
+
+	Scanner sc = new Scanner(System.in);
+
+	int []i = new int [4];
+
+	i[0]= sc.nextInt();
+
+	i[1]= sc.nextInt();
+
+	i[2]= sc.nextInt();
+
+	i[3]= sc.nextInt();
+
+	System.out.println(Arrays.toString(i));
+
+
+
+	}
+}
