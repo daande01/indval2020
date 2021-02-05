8a6789b83f73b59f61994bdc35162409f4d097f6
daande1 <danielevertandersson>
2021-02-05 10:48:16 +0100

    new file

--- programmering1_indval_2020/src/loopar/repetition3.java
+++ programmering1_indval_2020/src/loopar/repetition3.java
@@ -7,53 +7,60 @@ public class repetition3 {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
+		int val=0;
+		do {
 
-		System.out.println("1.boka tid \n2.ange regnummer \n3.av boka");
+			System.out.println("1.boka tid \n2.ange regnummer \n3.av boka \n4.avsluta");
 
-		int val = sc.nextInt();
+			 val = sc.nextInt();
 
-		switch (val) {
-		case 1:
+			switch (val) {
+			case 1:
 
-			repetition3.bokaTid();
+				repetition3.bokaTid();
 
-			break;
-		case 2:
+				break;
+			case 2:
 
-			angeReg();
+				angeReg();
 
-			break;
+				break;
 
-		case 3:
+			case 3:
 
-			avBoka();
+				avBoka();
 
-			break;
-		default:
+				break;
+			case 4:
 
-			break;
-		}
+				System.out.println("välkommen åter");
 
+				break;
+			default:
+
+				break;
+			}
+
+		} while (val !=4);
 
 	}
 
 	public static void bokaTid() {
 
-
 		System.out.println("du har valt boka tid");
 
-
 	}
+
 	public static void angeReg() {
 
 		System.out.println("ange reg");
 
 	}
+
 	public static void avBoka() {
 
+		System.out.println("avboka");
 
 	}
-
-
 
 }
---
+++ programmering1_indval_2020/src/loopar/repetition4.java
@@ -0,0 +1,35 @@
+package loopar;
+
+public class repetition4 {
+
+	public static void main(String[] args) {
+
+	summa(6,4);
+
+	int prod =produkt(5,5);
+
+	System.out.println(prod);
+	}
+
+
+	public static void summa (int a, int b ) {
+
+		int sum= a+b;
+		System.out.println(sum);
+
+	}
+
+	public static int produkt(int a, int b) {
+
+
+
+		return a*b;
+
+	}
+
+
+
+
+
+
+}
