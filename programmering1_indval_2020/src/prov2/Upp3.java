736b17a394976a1aabb34997dd33c456ab9b57d7
daande1 <danielevertandersson>
2021-03-05 10:45:19 +0100

    new file

--- programmering1_indval_2020/src/prov2/Upp3.java
+++ programmering1_indval_2020/src/prov2/Upp3.java
@@ -1,33 +1,25 @@
 package prov2;
 
+import javax.swing.JOptionPane;
+
 public class Upp3 {
-
-
-
-
 
 	public static void main(String[] args) {
 
+		int år = 0;
 
-		int maskinvärde =500000;
+		String stringVärde = JOptionPane.showInputDialog("angemaskin värde vid inköp");
 
+		int maskinVärde = Integer.parseInt(stringVärde);
 
+		// skapa loop som avslutas när maskinvärde <1000 while
 
+				// minska maskinvärde med 20%
+				// år ++
+		//
 
-
-		while() {
-
-
-
-
-
-		}
-
-
-
-
+		// skriv ut antal år
 
 	}
-
 
 }
